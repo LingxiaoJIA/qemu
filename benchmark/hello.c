594c2a22778a983648223dbe6563a39ea1d87d92
Lingxiao Jia <lingxiao.jia@utexas.edu>
2015-07-30 16:19:43 -0500

    benchmark: Modify hello.c

--- benchmark/hello.c
+++ benchmark/hello.c
@@ -2,7 +2,10 @@
 
 int main()
 {
-    printf("Hello World!\n");
+    FILE *fptr = fopen("/home/rexjia/qemu/temp/in_dats/dat_xxx.dat", "w");
+    assert(fptr != NULL);
+    fprintf(stdout, "Hello World!\n");
+    fclose(fptr);
 
     return 0;
 }
