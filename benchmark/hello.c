9a78cecf2cb34186be81b83ff6622cf6650d4e57
Lingxiao Jia <lingxiao.jia@utexas.edu>
2015-08-07 16:44:44 -0500

    benchmark: Updated benchmark files

--- benchmark/hello.c
+++ benchmark/hello.c
@@ -2,10 +2,7 @@
 
 int main()
 {
-    FILE *fptr = fopen("/home/rexjia/qemu/temp/in_dats/dat_xxx.dat", "w");
-    assert(fptr != NULL);
     fprintf(stdout, "Hello World!\n");
-    fclose(fptr);
 
     return 0;
 }
